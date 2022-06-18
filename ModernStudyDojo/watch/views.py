8044c2c4c4587bd6d2f28019cac1294269b7659d
Tommy <tommylam979@gmail.com>
2022-06-19 00:25:03 +0200

    login

--- ModernStudyDojo/watch/views.py
+++ ModernStudyDojo/watch/views.py
@@ -1,12 +1,23 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .models import *
+from .forms import OrderForm
+from .filters import OrderFilter
+
+
 def watch(request, *args, **kwargs):
     return render(request, "watch.html", {})
 
 def index(request, *args, **kwargs):
     return render(request, "index.html", {})
 
-def login(request, *args, **kwargs):
-    return render(request, "login.html", {})
+def login(request):
+    form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, "login.html", context)
